fc8dc903b7a2f9461e42d76234f34e2b6cf8e1e8
John Youren <john.youren@gmail.com>
2018-05-08 12:02:17 +0100

    Added fix for columns not in schema

--- src/ESFA.DC.DatabaseTesting.Tests/TestDatabase.cs
+++ src/ESFA.DC.DatabaseTesting.Tests/TestDatabase.cs
@@ -9,7 +9,7 @@ namespace ESFA.DC.DatabaseTesting.Tests
     public class TestDatabase
     {
         [Fact]
-        public void TestSchema()
+        public void TestSchemaExists()
         {
             IDbConnectorConfiguration dbConnectorConfiguration = new DbConnectorConfiguration(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
             IDbConnector dbConnector = new DbConnector(dbConnectorConfiguration);
@@ -25,6 +25,25 @@ namespace ESFA.DC.DatabaseTesting.Tests
                 ExpectedColumn.CreateDateTime("aDateTime", 6, false)
             };
             schemaTests.AssertTableColumnsExist("dbo", "TestTable", expectedColumns, true);
+        }
+
+        [Fact]
+        public void TestSchemaNotExists()
+        {
+            IDbConnectorConfiguration dbConnectorConfiguration = new DbConnectorConfiguration(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
+            IDbConnector dbConnector = new DbConnector(dbConnectorConfiguration);
+            ISchemaTests schemaTests = new SchemaTests(dbConnector);
+
+            IExpectedColumn[] expectedColumns =
+            {
+                ExpectedColumn.CreateBigInt("IDa", 1, false),
+                ExpectedColumn.CreateInt("anIntB", 2, false),
+                ExpectedColumn.CreateNvarChar("aStringC", 3, false),
+                ExpectedColumn.CreateBit("aBoolD", 4, false),
+                ExpectedColumn.CreateDecimal("aDecimalE", 5, false),
+                ExpectedColumn.CreateDateTime("aDateTimeF", 6, false)
+            };
+            schemaTests.AssertTableColumnsExist("dbo", "TestTable", expectedColumns, false);
         }
     }
 }
